fb77e2a1d6d886ffc2ffaba56efbc211ce261a3b
ClaudioCamba <33432769+ClaudioCamba@users.noreply.github.com>
2024-03-11 21:23:16 +0000

    update

--- src/components/songs.jsx
+++ src/components/songs.jsx
@@ -1,31 +1,17 @@
 import CurrentSong from "./currentsong";
 import NumberOfPlays from "./numberofplays";
 
-const ChangeSong = ({current,change,songsArray}) => {
-    return (
-        <button onClick={()=>{
-          if(current === songsArray.length-1){
-            current = -1;
-          }
-          change(current + 1);
-        }} >
-          Next Track
-        </button>
-    )
-  }
-
 const Songs = ({songsArray, playing, playCounts, changeSong}) => {
     return (
         <>
 
     <button onClick={()=>{
-          if(playing === songsArray.length-1){
-            playing = -1;
-          }
+          if(playing === songsArray.length-1) playing = -1;
           changeSong(playing + 1);
         }} >
           Next Track
     </button>
+
       <ul className="song-list">
           {songsArray.map((song) => {
             return <li className="song-item" key={song.title}>
